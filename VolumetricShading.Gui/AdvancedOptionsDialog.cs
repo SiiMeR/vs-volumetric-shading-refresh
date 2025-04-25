769103868fe163e287f149e6c9ff81e826d16eda
Siim Raudsepp <siimraudsepp@msn.com>
2025-04-26 02:03:54 +0300

    Fix the config

--- VolumetricShading.Gui/AdvancedOptionsDialog.cs
+++ VolumetricShading.Gui/AdvancedOptionsDialog.cs
@@ -4,114 +4,133 @@ using System.Linq;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 
-namespace volumetricshadingupdated.VolumetricShading.Gui;
-
-public abstract class AdvancedOptionsDialog : GuiDialog
+namespace volumetricshadingupdated.VolumetricShading.Gui
 {
-    public class ConfigOption
-    {
-        public string SwitchKey;
+	// Token: 0x02000031 RID: 49
+	public abstract class AdvancedOptionsDialog : GuiDialog
+	{
+		// Token: 0x0600015E RID: 350 RVA: 0x0000307A File Offset: 0x0000127A
+		protected AdvancedOptionsDialog(ICoreClientAPI capi)
+			: base(capi)
+		{
+		}
 
-        public string SliderKey;
+		// Token: 0x0600015F RID: 351 RVA: 0x0000308E File Offset: 0x0000128E
+		protected void RegisterOption(AdvancedOptionsDialog.ConfigOption option)
+		{
+			this.ConfigOptions.Add(option);
+		}
 
-        public string Text;
+		// Token: 0x06000160 RID: 352 RVA: 0x00006234 File Offset: 0x00004434
+		protected void SetupDialog()
+		{
+			this._isSetup = true;
+			ElementBounds dialogBounds = ElementStdBounds.AutosizedMainDialog.WithAlignment(EnumDialogArea.RightBottom).WithFixedAlignmentOffset(-GuiStyle.DialogToScreenPadding, -GuiStyle.DialogToScreenPadding);
+			CairoFont font = CairoFont.WhiteSmallText();
+			ElementBounds switchBounds = ElementBounds.Fixed(250.0, GuiStyle.TitleBarHeight, 20.0, 20.0);
+			ElementBounds textBounds = ElementBounds.Fixed(0.0, GuiStyle.TitleBarHeight + 1.0, 240.0, 20.0);
+			ElementBounds bgBounds = ElementBounds.Fill.WithFixedPadding(GuiStyle.ElementToDialogPadding);
+			bgBounds.BothSizing = ElementSizing.FitToChildren;
+			GuiComposer composer = this.capi.Gui.CreateCompo(this.DialogKey, dialogBounds).AddShadedDialogBG(bgBounds, true, 5.0, 0.75f).AddDialogTitleBar(this.DialogTitle, new Action(this.OnTitleBarCloseClicked), null, null)
+				.BeginChildElements(bgBounds);
+			foreach (AdvancedOptionsDialog.ConfigOption option3 in this.ConfigOptions)
+			{
+				composer.AddStaticText(option3.Text, font, textBounds, null);
+				if (option3.Tooltip != null)
+				{
+					composer.AddHoverText(option3.Tooltip, font, 260, textBounds, null);
+				}
+				if (option3.SliderKey != null)
+				{
+					composer.AddSlider(option3.SlideAction, switchBounds.FlatCopy().WithFixedWidth(200.0), option3.SliderKey);
+				}
+				else if (option3.SwitchKey != null)
+				{
+					composer.AddSwitch(option3.ToggleAction, switchBounds, option3.SwitchKey, 20.0, 4.0);
+				}
+				textBounds = textBounds.BelowCopy(0.0, 10.0, 0.0, 0.0);
+				switchBounds = switchBounds.BelowCopy(0.0, 10.0, 0.0, 0.0);
+			}
+			base.SingleComposer = composer.EndChildElements().Compose(true);
+			foreach (AdvancedOptionsDialog.ConfigOption option2 in this.ConfigOptions.Where((AdvancedOptionsDialog.ConfigOption option) => option.SliderKey != null && !option.InstantSlider))
+			{
+				base.SingleComposer.GetSlider(option2.SliderKey).TriggerOnlyOnMouseUp(true);
+			}
+		}
 
-        public string Tooltip;
+		// Token: 0x06000161 RID: 353 RVA: 0x0000309C File Offset: 0x0000129C
+		public override bool TryOpen()
+		{
+			if (!this._isSetup)
+			{
+				this.SetupDialog();
+			}
+			if (!base.TryOpen())
+			{
+				return false;
+			}
+			VolumetricShadingMod.Instance.CurrentDialog = this;
+			this.RefreshValues();
+			return true;
+		}
 
-        public Action<bool> ToggleAction;
+		// Token: 0x1700004C RID: 76
+		// (get) Token: 0x06000162 RID: 354
+		protected abstract string DialogKey { get; }
 
-        public ActionConsumable<int> SlideAction;
+		// Token: 0x1700004D RID: 77
+		// (get) Token: 0x06000163 RID: 355
+		protected abstract string DialogTitle { get; }
 
-        public bool InstantSlider;
-    }
+		// Token: 0x06000164 RID: 356
+		protected abstract void RefreshValues();
 
-    protected List<ConfigOption> ConfigOptions = new List<ConfigOption>();
+		// Token: 0x06000165 RID: 357 RVA: 0x000030C8 File Offset: 0x000012C8
+		protected void OnTitleBarCloseClicked()
+		{
+			this.TryClose();
+			VolumetricShadingMod.Instance.ConfigGui.TryOpen();
+		}
 
-    private bool _isSetup;
+		// Token: 0x1700004E RID: 78
+		// (get) Token: 0x06000166 RID: 358 RVA: 0x000030E1 File Offset: 0x000012E1
+		public override string ToggleKeyCombinationCode
+		{
+			get
+			{
+				return null;
+			}
+		}
 
-    protected abstract string DialogKey { get; }
+		// Token: 0x040000A0 RID: 160
+		protected List<AdvancedOptionsDialog.ConfigOption> ConfigOptions = new List<AdvancedOptionsDialog.ConfigOption>();
 
-    protected abstract string DialogTitle { get; }
+		// Token: 0x040000A1 RID: 161
+		private bool _isSetup;
 
-    public override string ToggleKeyCombinationCode => null;
+		// Token: 0x02000032 RID: 50
+		public class ConfigOption
+		{
+			// Token: 0x040000A2 RID: 162
+			public string SwitchKey;
 
-    protected AdvancedOptionsDialog(ICoreClientAPI capi)
-        : base(capi)
-    {
-    }
+			// Token: 0x040000A3 RID: 163
+			public string SliderKey;
 
-    protected void RegisterOption(ConfigOption option)
-    {
-        ConfigOptions.Add(option);
-    }
+			// Token: 0x040000A4 RID: 164
+			public string Text;
 
-    protected void SetupDialog()
-    {
-        _isSetup = true;
-        ElementBounds val = ElementStdBounds.AutosizedMainDialog.WithAlignment((EnumDialogArea)11)
-            .WithFixedAlignmentOffset(0.0 - GuiStyle.DialogToScreenPadding, 0.0 - GuiStyle.DialogToScreenPadding);
-        CairoFont val2 = CairoFont.WhiteSmallText();
-        ElementBounds val3 = ElementBounds.Fixed(250.0, GuiStyle.TitleBarHeight, 20.0, 20.0);
-        ElementBounds val4 = ElementBounds.Fixed(0.0, GuiStyle.TitleBarHeight + 1.0, 240.0, 20.0);
-        ElementBounds val5 = ElementBounds.Fill.WithFixedPadding(GuiStyle.ElementToDialogPadding);
-        val5.BothSizing = (ElementSizing)2;
-        GuiComposer val6 = GuiComposerHelpers
-            .AddDialogTitleBar(
-                GuiComposerHelpers.AddShadedDialogBG(base.capi.Gui.CreateCompo(DialogKey, val), val5, true, 5.0, 0.75f),
-                DialogTitle, (Action)OnTitleBarCloseClicked, (CairoFont)null, (ElementBounds)null)
-            .BeginChildElements(val5);
-        foreach (ConfigOption configOption in ConfigOptions)
-        {
-            GuiComposerHelpers.AddStaticText(val6, configOption.Text, val2, val4, (string)null);
-            if (configOption.Tooltip != null)
-            {
-                GuiComposerHelpers.AddHoverText(val6, configOption.Tooltip, val2, 260, val4, (string)null);
-            }
+			// Token: 0x040000A5 RID: 165
+			public string Tooltip;
 
-            if (configOption.SliderKey != null)
-            {
-                GuiComposerHelpers.AddSlider(val6, configOption.SlideAction, val3.FlatCopy().WithFixedWidth(200.0),
-                    configOption.SliderKey);
-            }
-            else if (configOption.SwitchKey != null)
-            {
-                GuiComposerHelpers.AddSwitch(val6, configOption.ToggleAction, val3, configOption.SwitchKey, 20.0, 4.0);
-            }
+			// Token: 0x040000A6 RID: 166
+			public Action<bool> ToggleAction;
 
-            val4 = val4.BelowCopy(0.0, 10.0, 0.0, 0.0);
-            val3 = val3.BelowCopy(0.0, 10.0, 0.0, 0.0);
-        }
+			// Token: 0x040000A7 RID: 167
+			public ActionConsumable<int> SlideAction;
 
-        ((GuiDialog)this).SingleComposer = val6.EndChildElements().Compose(true);
-        foreach (ConfigOption item in ConfigOptions.Where((ConfigOption option) =>
-                     option.SliderKey != null && !option.InstantSlider))
-        {
-            GuiComposerHelpers.GetSlider(((GuiDialog)this).SingleComposer, item.SliderKey).TriggerOnlyOnMouseUp();
-        }
-    }
-
-    public override bool TryOpen()
-    {
-        if (!_isSetup)
-        {
-            SetupDialog();
-        }
-
-        if (!((GuiDialog)this).TryOpen())
-        {
-            return false;
-        }
-
-        VolumetricShadingMod.Instance.CurrentDialog = (GuiDialog)(object)this;
-        RefreshValues();
-        return true;
-    }
-
-    protected abstract void RefreshValues();
-
-    protected void OnTitleBarCloseClicked()
-    {
-        ((GuiDialog)this).TryClose();
-        ((GuiDialog)VolumetricShadingMod.Instance.ConfigGui).TryOpen();
-    }
+			// Token: 0x040000A8 RID: 168
+			public bool InstantSlider;
+		}
+	}
 }
--- VolumetricShading.Gui/MainConfigDialog.cs
+++ VolumetricShading.Gui/MainConfigDialog.cs
@@ -3,100 +3,106 @@ using System.Collections.Generic;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 
-namespace volumetricshadingupdated.VolumetricShading.Gui;
-
-public abstract class MainConfigDialog : GuiDialog
+namespace volumetricshadingupdated.VolumetricShading.Gui
 {
-    public class ConfigOption
-    {
-        public ActionConsumable AdvancedAction;
+	// Token: 0x02000034 RID: 52
+	public abstract class MainConfigDialog : GuiDialog
+	{
+		// Token: 0x0600016B RID: 363 RVA: 0x00003105 File Offset: 0x00001305
+		protected MainConfigDialog(ICoreClientAPI capi)
+			: base(capi)
+		{
+		}
 
-        public string SwitchKey;
+		// Token: 0x0600016C RID: 364 RVA: 0x00003119 File Offset: 0x00001319
+		protected void RegisterOption(MainConfigDialog.ConfigOption option)
+		{
+			this.ConfigOptions.Add(option);
+		}
 
-        public string Text;
+		// Token: 0x0600016D RID: 365 RVA: 0x00006504 File Offset: 0x00004704
+		protected void SetupDialog()
+		{
+			this._isSetup = true;
+			ElementBounds dialogBounds = ElementStdBounds.AutosizedMainDialog.WithAlignment(EnumDialogArea.RightBottom).WithFixedAlignmentOffset(-GuiStyle.DialogToScreenPadding, -GuiStyle.DialogToScreenPadding);
+			CairoFont font = CairoFont.WhiteSmallText();
+			ElementBounds switchBounds = ElementBounds.Fixed(210.0, GuiStyle.TitleBarHeight, 20.0, 20.0);
+			ElementBounds textBounds = ElementBounds.Fixed(0.0, GuiStyle.TitleBarHeight + 1.0, 200.0, 20.0);
+			ElementBounds advancedButtonBounds = ElementBounds.Fixed(240.0, GuiStyle.TitleBarHeight, 110.0, 20.0);
+			ElementBounds bgBounds = ElementBounds.Fill.WithFixedPadding(GuiStyle.ElementToDialogPadding);
+			bgBounds.BothSizing = ElementSizing.FitToChildren;
+			GuiComposer composer = this.capi.Gui.CreateCompo("volumetricShadingConfigure", dialogBounds).AddShadedDialogBG(bgBounds, true, 5.0, 0.75f).AddDialogTitleBar("Volumetric Shading Configuration", new Action(this.OnTitleBarCloseClicked), null, null)
+				.BeginChildElements(bgBounds);
+			foreach (MainConfigDialog.ConfigOption option in this.ConfigOptions)
+			{
+				composer.AddStaticText(option.Text, font, textBounds, null);
+				if (option.Tooltip != null)
+				{
+					composer.AddHoverText(option.Tooltip, font, 250, textBounds.FlatCopy(), null);
+				}
+				if (option.SwitchKey != null)
+				{
+					composer.AddSwitch(option.ToggleAction, switchBounds, option.SwitchKey, 20.0, 4.0);
+				}
+				if (option.AdvancedAction != null)
+				{
+					composer.AddSmallButton("Advanced...", option.AdvancedAction, advancedButtonBounds, EnumButtonStyle.Normal, null);
+				}
+				switchBounds = switchBounds.BelowCopy(0.0, 10.0, 0.0, 0.0);
+				textBounds = textBounds.BelowCopy(0.0, 10.0, 0.0, 0.0);
+				advancedButtonBounds = advancedButtonBounds.BelowCopy(0.0, 10.0, 0.0, 0.0);
+			}
+			base.SingleComposer = composer.EndChildElements().Compose(true);
+		}
 
-        public Action<bool> ToggleAction;
+		// Token: 0x0600016E RID: 366 RVA: 0x00003127 File Offset: 0x00001327
+		public override bool TryOpen()
+		{
+			if (!this._isSetup)
+			{
+				this.SetupDialog();
+			}
+			if (!base.TryOpen())
+			{
+				return false;
+			}
+			this.RefreshValues();
+			VolumetricShadingMod.Instance.CurrentDialog = this;
+			return true;
+		}
 
-        public string Tooltip;
-    }
+		// Token: 0x0600016F RID: 367 RVA: 0x00003153 File Offset: 0x00001353
+		private void OnTitleBarCloseClicked()
+		{
+			this.TryClose();
+		}
 
-    private bool _isSetup;
+		// Token: 0x06000170 RID: 368
+		protected abstract void RefreshValues();
 
-    protected List<ConfigOption> ConfigOptions = new List<ConfigOption>();
+		// Token: 0x040000AB RID: 171
+		private bool _isSetup;
 
-    protected MainConfigDialog(ICoreClientAPI capi)
-        : base(capi)
-    {
-    }
+		// Token: 0x040000AC RID: 172
+		protected List<MainConfigDialog.ConfigOption> ConfigOptions = new List<MainConfigDialog.ConfigOption>();
 
-    protected void RegisterOption(ConfigOption option)
-    {
-        ConfigOptions.Add(option);
-    }
+		// Token: 0x02000035 RID: 53
+		public class ConfigOption
+		{
+			// Token: 0x040000AD RID: 173
+			public ActionConsumable AdvancedAction;
 
-    protected void SetupDialog()
-    {
-        _isSetup = true;
-        ElementBounds val = ElementStdBounds.AutosizedMainDialog.WithAlignment((EnumDialogArea)11)
-            .WithFixedAlignmentOffset(0.0 - GuiStyle.DialogToScreenPadding, 0.0 - GuiStyle.DialogToScreenPadding);
-        CairoFont val2 = CairoFont.WhiteSmallText();
-        ElementBounds val3 = ElementBounds.Fixed(210.0, GuiStyle.TitleBarHeight, 20.0, 20.0);
-        ElementBounds val4 = ElementBounds.Fixed(0.0, GuiStyle.TitleBarHeight + 1.0, 200.0, 20.0);
-        ElementBounds val5 = ElementBounds.Fixed(240.0, GuiStyle.TitleBarHeight, 110.0, 20.0);
-        ElementBounds val6 = ElementBounds.Fill.WithFixedPadding(GuiStyle.ElementToDialogPadding);
-        val6.BothSizing = (ElementSizing)2;
-        GuiComposer val7 = GuiComposerHelpers.AddDialogTitleBar(
-            GuiComposerHelpers.AddShadedDialogBG(base.capi.Gui.CreateCompo("volumetricShadingConfigure", val), val6,
-                true, 5.0, 0.75f), "Volumetric Shading Configuration", (Action)OnTitleBarCloseClicked, (CairoFont)null,
-            (ElementBounds)null).BeginChildElements(val6);
-        foreach (ConfigOption configOption in ConfigOptions)
-        {
-            GuiComposerHelpers.AddStaticText(val7, configOption.Text, val2, val4, (string)null);
-            if (configOption.Tooltip != null)
-            {
-                GuiComposerHelpers.AddHoverText(val7, configOption.Tooltip, val2, 250, val4.FlatCopy(), (string)null);
-            }
+			// Token: 0x040000AE RID: 174
+			public string SwitchKey;
 
-            if (configOption.SwitchKey != null)
-            {
-                GuiComposerHelpers.AddSwitch(val7, configOption.ToggleAction, val3, configOption.SwitchKey, 20.0, 4.0);
-            }
+			// Token: 0x040000AF RID: 175
+			public string Text;
 
-            if (configOption.AdvancedAction != null)
-            {
-                GuiComposerHelpers.AddSmallButton(val7, "Advanced...", configOption.AdvancedAction, val5,
-                    (EnumButtonStyle)2, (string)null);
-            }
+			// Token: 0x040000B0 RID: 176
+			public Action<bool> ToggleAction;
 
-            val3 = val3.BelowCopy(0.0, 10.0, 0.0, 0.0);
-            val4 = val4.BelowCopy(0.0, 10.0, 0.0, 0.0);
-            val5 = val5.BelowCopy(0.0, 10.0, 0.0, 0.0);
-        }
-
-        ((GuiDialog)this).SingleComposer = val7.EndChildElements().Compose(true);
-    }
-
-    public override bool TryOpen()
-    {
-        if (!_isSetup)
-        {
-            SetupDialog();
-        }
-
-        if (!((GuiDialog)this).TryOpen())
-        {
-            return false;
-        }
-
-        RefreshValues();
-        VolumetricShadingMod.Instance.CurrentDialog = (GuiDialog)(object)this;
-        return true;
-    }
-
-    private void OnTitleBarCloseClicked()
-    {
-        ((GuiDialog)this).TryClose();
-    }
-
-    protected abstract void RefreshValues();
+			// Token: 0x040000B1 RID: 177
+			public string Tooltip;
+		}
+	}
 }
