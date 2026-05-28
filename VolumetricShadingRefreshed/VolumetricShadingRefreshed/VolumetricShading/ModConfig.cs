a49f5274888472191dae1077410cff8a0d78ba95
Siim Raudsepp <siimraudsepp@msn.com>
2026-05-28 20:52:38 +0300

    Settings changes

--- VolumetricShadingRefreshed/VolumetricShadingRefreshed/VolumetricShading/ModConfig.cs
+++ VolumetricShadingRefreshed/VolumetricShadingRefreshed/VolumetricShading/ModConfig.cs
@@ -2,20 +2,20 @@ namespace VolumetricShadingRefreshed.VolumetricShading;
 
 public class ModConfig
 {
-    public bool ScreenSpaceReflectionsEnabled { get; set; } = false;
+    public bool ScreenSpaceReflectionsEnabled { get; set; } = true;
     public bool SSRRainReflectionsEnabled { get; set; } = true;
     public bool SSRRefractionsEnabled { get; set; } = true;
     public bool SSRCausticsEnabled { get; set; } = true;
     public int SSRWaterTransparency { get; set; } = 25;
     public int SSRSplashTransparency { get; set; } = 55;
-    public int SSRReflectionDimming { get; set; } = 110;
-    public int SSRTintInfluence { get; set; } = 35;
-    public int SSRSkyMixin { get; set; } = 10;
-    public int SSRDistortion { get; set; } = 30;
+    public int SSRReflectionDimming { get; set; } = 130;
+    public int SSRTintInfluence { get; set; } = 10;
+    public int SSRSkyMixin { get; set; } = 12;
+    public int SSRDistortion { get; set; } = 5;
     public int SSRStrength { get; set; } = 100;
 
     public int VolumetricLightingFlatness { get; set; } = 120;
-    public int VolumetricLightingIntensity { get; set; } = 30;
+    public int VolumetricLightingIntensity { get; set; } = 36;
 
     public int OverexposureIntensity { get; set; } = 0;
     public int SunBloomIntensity { get; set; } = 0;
@@ -27,7 +27,7 @@ public class ModConfig
     public int FarPeterPanningAdjustment { get; set; } = 5;
 
     public bool SSDOEnabled { get; set; } = false;
-    public bool DeferredLightingEnabled { get; set; } = false;
+    public bool DeferredLightingEnabled { get; set; } = true;
     public bool UnderwaterTweaksEnabled { get; set; } = true;
 
     public bool DepthOfFieldEnabled { get; set; } = false;
